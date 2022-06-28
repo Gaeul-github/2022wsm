db147a3c7f908d68de36922c8a998ae5a3eb20ad
gaeul <chlrkdmf0805@naver.com>
2022-06-28 15:48:43 +0900

    js  조식 중식 석식 시간별로 나타나게

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -29,28 +29,37 @@ const addNow = (homeCardId) =>{
     const homeCard = document.getElementById(homeCardId);
 
     //시간 -> index번째 식사가 선택되어야하는지 결정
-    //조식 끝 : 8 : 00
-    //중식 끝 : 13 : 00
-    //석식 끝 : 17 : 50
+    //조식 끝 : 8 : 00 -> 480
+    //중식 끝 : 13 : 00 -> 780
+    //석식 끝 : 17 : 50 -> 1070
     var now = new Date();
     let hour = now.getHours();
     let minute = now.getMinutes();
     console.log(hour , minute);
     // index = hour%12;
-    hour = 18;
-    minute = 0;
-    if(minute >= 50){
-        hour = hour + 1;
+    // hour = 4;
+    // minute = 20;
+    // console.log(hour, minute);
+
+    // index = 0;
+    // console.log(index);
+
+    minute = hour*60+minute;
+
+    if(minute > 1070){
+        index = 0;
+        console.log(index);
+    }else if(minute > 780){
+        index = 2;
+        console.log(index);
+    }else if(minute > 480){
+        index = 1;
+        console.log(index);
     }else{
-        hour = hour - 1;
+        index = 0;
+        console.log(index);
     }
-    if(hour >= 8 && hour <= 13){ //중식
-        index=1;
-    }else if(hour > 13 && hour <= 18){ //석식
-        index=2;
-    }else{
-        index=0;
-    }
+    
     //homeCard에서 index번째 card에 new 클래스 추가
     let card =  homeCard.getElementsByClassName('card')[index];
     card.classList.add('now');
